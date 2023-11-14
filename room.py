f2e11d6dbcd3bdd71d7595d29ac4cb117b237f7a
Adnan Ali <62577824+adnanali88@users.noreply.github.com>
2023-11-14 18:27:19 -0500

    Update room.py

--- room.py
+++ room.py
@@ -1,18 +1,27 @@
 class Room:
-    def __init__(self, room_id):
-        self.room_id = room_id
-        self.connected_rooms = {}
-        self.connected_doors = {}
+    def __init__(self, room_name,connected_to,associated_door,initial_inventory,character):
+        self.room_name = room_name
+        self.connected_to = connected_to
+        self.associated_door = associated_door
+        self.initial_inventory=initial_inventory
+        self.characters_inside=[]
         self.state = None
 
     def getRoom(self):
         return self.room_id
 
     def getDoor(self):
-        return self.connected_doors
+        return self.connected_to
 
     def getState(self):
         return self.state
 
     def setState(self, new_state):
         self.state = new_state
+
+
+    def __str__(self):
+        return f"{self.room_name}"
+    
+    def __repr__(self):
+        return f"{self.room_name}"
