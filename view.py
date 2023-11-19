1d709e8435f9aff7c5f961a055e80a489d0b7b98
unknown <96yrlee@gmail.com>
2023-11-18 19:27:25 -0500

    updated view.py - input tested, output still testing and fixing

--- view.py
+++ view.py
@@ -1,36 +1,63 @@
-class InputHandler:
-    def __init__(self) -> None:
-        # format is Verb Target with Item - ex) attack grunt with blaster
-        self.reset()
-        # default prompt
-        self.prompt = "What will you do? >"
+import re
 
-    # this methods will extract the key words from the given user Input
+
+class InputHandler:
+    def __init__(
+        self, prompt="What will you do? >", invalidResponse="Invalid input."
+    ) -> None:
+        """
+        Verb Target Item ex) attack grunt with blaster
+        Verb Item Target ex) use ITEM on SOMETHING
+        => caller needs to know how to parse which is target and which is item based on Verb
+        """
+
+        # code repeat with reset() but better readibility
+        self.verb = ""
+        self.keyword2 = ""
+        self.keyword1 = ""
+        self.prompt = prompt
+        self.invalidResponse = invalidResponse
+
+    # this method will extract the verb/command, and keywords from the user Input
     def parseInput(self):
         self.reset()  # ensures prev parsed input does not linger
 
-        rawInput = input(self.prompt).lower().split(" ")
+        # imitation do-while loop to ensure valid input from user
+        while True:
+            # recall that: - strip() trims whitespace at front and end of string
+            #            : - split() list-ifys a string based on a given seperator
+            rawInput = input(self.prompt).strip().lower().split(" ")
 
+            # basic error check : may create a new function for this later
+            if rawInput[0] == "" or len(rawInput) > 4:
+                print(self.invalidResponse)
+            else:
+                break
+
+        # min valid input is a verb. target and item are not always there
         self.verb = rawInput[0]
-        self.target = rawInput[1]
+        if len(rawInput) > 1:
+            self.keyword1 = rawInput[1]
         if len(rawInput) > 2:
-            self.item = rawInput[-1]
+            self.keyword2 = rawInput[-1]
 
     def getVerb(self):
         return self.verb
 
-    def getTarget(self):
-        return self.target
+    def getFirstKeyword(self):
+        return self.keyword1
 
-    def getItem(self):
-        return self.item
+    def getSecondKeyword(self):
+        return self.keyword2
 
     # can change the prompt per situation
     def setPrompt(self, givenPrompt):
         self.prompt = givenPrompt
 
     def reset(self):
-        self.verb, self.target, self.item = ""
+        self.verb = ""
+        self.keyword2 = ""
+        self.keyword1 = ""
 
 
 class OutputHandler:
@@ -41,24 +68,40 @@ class OutputHandler:
         print(self.buffer)
         self.clearBuffer()
 
-    # even is there is only 1 object to print, must be in a list for paras
-    def formatOutput(self, msg, paras):
-        if len(paras) > 1:
-            # will give index of spot to insert paras
-            position = msg.find("{}")
-            front = msg[:position]
-            back = msg[position:]
-
-            formattedParas = ""
-            for i in range(0, len(paras) - 2):
-                formattedParas += ", " + paras[i]
-            formattedParas += ", and " + paras[-1]
-
-            msg = front + formattedParas + back  # may need to add a space???
-
-        # 1 or more parameters, 1st will always be at default
+    # method assumes list is a homogeneous list of Strings
+    def listToGrammarString(self, list):
+        if len(list) > 1:
+            formatted = ", ".join(list[:-1]) + ", and " + list[-1]
         else:
-            msg.format(paras[0])
+            formatted = list[0]
+
+        print(formatted)
+        return formatted
+
+    """
+    two types of msgs that needs to be formatted: 
+        1) "You blah <> with/on <>" 
+        2) "blah blah <> blah"
+
+    1st will only have 1 THING per <>
+        => can use normal python formatter and nothing else
+
+    2nd can have many objects per <>
+    """
+
+    # even is there is only 1 object to print, must be in a list for para(meter)s
+    def formatOutput(self, msg, paras):
+        position = re.findall("<>", msg)
+        formatted = self.listToGrammarString(paras)
+
+        print(position, formatted)
+
+        if len(position) > 1:  # AKA more than one "<>"
+            front = msg[: position[0]]
+            back = msg[position[0] :]
+            msg = front + formatted + back  # may need to add a space???
+        else:
+            msg.format(formatted)
 
         self.appendToBuffer(msg)
 
@@ -90,28 +133,17 @@ class OutputHandler:
     # Game over messages, or other game-mechanic msgs, like warning of the overal timer
 
 
-# this class is kinda... not useful
-# might be better to only have the handlers OR have it all in this View class
-class View:
-    # initialize handlers
-    def __init__(self) -> None:
-        self.inHandler = InputHandler()
-        self.outHandler = OutputHandler()
+""" testing InputHandler
+inH = InputHandler()
 
-        # default prompt
-        self.prompt = "What will you do? >"
+inH.parseInput()
+print("verb: " + inH.getVerb())
+print("1st: " + inH.getFirstKeyword())
+print("2nd: " + inH.getSecondKeyword())
+"""
+"""testing output Handler
+"""
 
-    # can change the prompt per situation
-    def setPrompt(self, givenPrompt):
-        self.prompt = givenPrompt
-
-    # retrives the input from the user after printing the Prompt
-    # caller will receive a list of strings containing the [verb, target, item]
-    def getInput(self):
-        ih = self.inHandler
-        ih.parseInput(self.prompt)
-        return [ih.getVerb(), ih.getTarget(), ih.getItem()]
-
-    def display(self, string):
-        # prints the output to the user
-        print(string)
+inO = OutputHandler()
+inO.formatOutput("You attacked <>", ["grunt"])
+inO.displayOutput()
