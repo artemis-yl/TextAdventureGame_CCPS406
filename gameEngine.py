3bd1d03afaed8dbfbb5cc35540e53164b89e0ec1
unknown <96yrlee@gmail.com>
2023-11-22 16:46:14 -0500

    made more notes on the command dictionary and method locations

--- gameEngine.py
+++ gameEngine.py
@@ -5,18 +5,18 @@ class GameEngine:
     # maybe change the obj calling the method depending where we actually store the damn methods
     # to use method,type COMMANDS[key]( parameter )
     COMMANDS = {
-        "N": current_room.N,
-        "S": current_room.S,
-        "E": current_room.E,
-        "W": current_room.W,
-        "ATTACK": target_NPC.attack,
-        "SCAN": current_room.scan,
-        "INVENTORY": player_NPC.listInventory,
+        "N": self.N,
+        "S": self.S,
+        "E": self.E,
+        "W": self.W,
+        "ATTACK": self.attack,
+        "SCAN": self.current_room.scan,
+        "INVENTORY": self.player.listInventory,
         "USE": player_NPC.use,
-        "TAKE": player_NPC.take,
-        "TALK": target_NPC.talk,
-        "LOCATION": blank.location,
-        "DISCARD": blank.discard,
+        "TAKE": self.player.take,  # (item_to_take)
+        "TALK": self.talk,
+        "LOCATION": self.location,
+        "DISCARD": self.player.discard,
     }
 
     # notes on where these methods should be....
@@ -26,14 +26,14 @@ class GameEngine:
     #  => needs to stored HERE in gameEngine
     # ATTACK needs the attacking NPC and target NPC => here in GameEngine
     # LOCATION tells you current room => her ein GameEngine
+    # TALK only work on NPC, but the NPC needs to be in the same room as player => here in gameEngine
+    # USE requires that the player has the item, and that the target is in the room => here in game engine
 
     # SCAN lists out everything inside the current room's inventory => should be in room.py
 
     # INVENTORY lists out player inventory => in NPC.py
-    # TALk only work on NPC => NPC.py
     # DISCARD is for player to remove an item => in NPC.py
-
-    # TAKE can only work on items => item.py
+    # TAKE can only work on items, but need to place into NPC inv => NPC.py
 
     def __init__(self) -> None:
         self.player_status = True
@@ -57,6 +57,8 @@ class GameEngine:
             self.msg_dict,
             self.command_dict,
         ) = self.model.loadGame()
+
+        self.player = self.npc_dict["npc_player"]
 
         self.inHandler = view.InputHandler(self.command_dict)
         self.outHandler = view.OutputHandler(self.command_dict, self.msg_dict)
