cbcbc5021c50ba01712a7733e49991bdd1c4871f
Yamah Jaleel <jaleel.y01@gmail.com>
2023-11-22 22:52:56 -0500

    Update to print data as dicts

--- main.py
+++ main.py
@@ -2,66 +2,62 @@ import gameLoader
 import gameSaver
 
 loader = gameLoader.GameLoader('npcs.json', 'room.json', 'items.json', 'puzzles.json', 'gameMsg.json')
-npc_list, room_list, item_list, puzzle_list, message_dict = loader.loadGame()
+npc_dict, room_dict, item_dict, puzzle_dict, message_dict = loader.loadGame()
 
 
 print("-" * 50)
 print("NPCS")
 print("-" * 50)
-for npc in npc_list:
-    print(f"NPC Name: {npc.npc_id}")
-    print(f"Current State: {npc.current_state}")
-    print(f"State Descriptions: {npc.possible_states}")
-    print(f"Active: {npc.is_active}")
-    print(f"Roaming: {npc.is_roaming}")
-    print(f"Initial Inventory: {npc.initial_inventory}")
-    print(f"Puzzle List: {npc.puzzle_list}")
+for npc_key, npc_instance in npc_dict.items():
+    print(f"NPC Name: {npc_instance.name}")
+    print(f"Current State: {npc_instance.current_state}")
+    print(f"Descriptions: {npc_instance.state_descriptions}")
+    print(f"Active: {npc_instance.is_active}")
+    print(f"Roaming: {npc_instance.is_roaming}")
+    print(f"Initial Inventory: {npc_instance.inventory}")
+    #print(f"Puzzle List: {npc_instance.puzzle_list}")  
     print(f"\n")
-
 
 print("-" * 50)
 print("ITEMS")
 print("-" * 50)
-for item in item_list:
-    print(f"Item Name: {item.getName()}")
-    print(f"Description: {item.getDescription()}")
-    print(f"Is Weapon: {item.isWeapon()}")
-    print(f"Is Shield: {item.isShield()}")
-    print(f"Is Teleport: {item.isTeleport()}")
-    print(f"Is Revive: {item.isRevive()}")
+for item_key, item_instance in item_dict.items():
+    print(f"Item Name: {item_instance.name}")
+    print(f"Current State: {item_instance.current_state}")
+    print(f"Descriptions: {item_instance.state_descriptions}")
+    print(f"Is Weapon: {item_instance.isWeapon()}")
+    print(f"Is Shield: {item_instance.isShield()}")
+    print(f"Is Teleport: {item_instance.isTeleport()}")
+    print(f"Is Revive: {item_instance.isRevive()}")
     print("\n")
-
 
 
 print("-" * 50)
 print("ROOMS")
 print("-" * 50)
-for room in room_list:
-    print(f"Room Name: {room.room_name}")
-    print(f"Description: {room.description}")
-    print(f"Connected To: {room.connected_to}")
-    print(f"Associated Door: {room.associated_door}")
-    print(f"Initial Inventory: {room.initial_inventory}")
-    print(f"Npcs: {room.npcs_inside}")
+for room_key, room_instance in room_dict.items():
+    print(f"Room Name: {room_instance.name}")
+    print(f"Description: {room_instance.state_descriptions}")
+    print(f"Connected To: {room_instance.connected_to}")
+    print(f"Associated Door: {room_instance.associated_door}")
+    print(f"Inventory: {room_instance.inventory}")
     print("\n")
 
 
 print("-" * 50)
 print("PUZZLES")
 print("-" * 50)
-for puzzle in puzzle_list:
-    print(f"Puzzle Name: {puzzle.name}")
-    print(f"Descriptions: {puzzle.stateDescriptions}")
-    print(f"Current State: {puzzle.currentState}")
-    print(f"Key: {puzzle.key}")
-    print(f"Verb: {puzzle.keyVerb}")
-    print(f"Hints: {puzzle.hints}")
-    print(f"SubPuzzles: {puzzle.subPuzzles}")
+for puzzle_key, puzzle_instance in puzzle_dict.items():
+    print(f"Puzzle Name: {puzzle_instance.name}")
+    print(f"Descriptions: {puzzle_instance.state_descriptions}")
+    print(f"Current State: {puzzle_instance.current_state}")
+    print(f"Key: {puzzle_instance.key}")
+    print(f"Verb: {puzzle_instance.keyVerb}")
     print("\n")
 
 print("-" * 50)
 print("MESSAGES")
 print("-" * 50)
-for key, value in message_dict.items():
-    print(f"{key}: {value}")
+for message_name, message_data in message_dict.items():
+    print(f"{message_name}: {message_data}")
     print("\n")
