534f39045d38aad1ebe85c123a482acf7c02d57d
unknown <96yrlee@gmail.com>
2023-11-23 14:07:15 -0500

    added SAVE and HELP commands, changing gameState with minor changes to gameEngine and gameLoader as result

--- gameLoader.py
+++ gameLoader.py
@@ -4,14 +4,16 @@ from room import Room
 from item import Item
 from puzzle import Puzzle
 
+
 class GameLoader:
-    def __init__(self, npc_filename, room_filename, items_filename, puzzles_filename, messages_filename):
+    def __init__(self, file_name_list):
         # Constructor to initialize the file names
-        self.npc_filename = npc_filename
-        self.room_filename = room_filename
-        self.items_filename = items_filename
-        self.puzzles_filename = puzzles_filename
-        self.messages_filename = messages_filename
+        self.npc_filename = file_name_list[0]
+        self.room_filename = file_name_list[1]
+        self.items_filename = file_name_list[2]
+        self.puzzles_filename = file_name_list[3]
+        self.messages_filename = file_name_list[4]
+        self.commands_filename = file_name_list[5]
 
     def loadGame(self):
         # Method to load the game data
@@ -20,12 +22,16 @@ class GameLoader:
         item_dict = self.import_items()
         puzzle_dict = self.import_puzzles()
         message_dict = self.import_messages()
+        cmd_dict = self.import_commands()
 
-        return [npc_dict, room_dict, item_dict, puzzle_dict, message_dict]
+        return [npc_dict, room_dict, item_dict, puzzle_dict, message_dict, cmd_dict]
+
+    def import_commands(self):
+        pass
 
     def import_npcs(self):
         # Method to import npcs
-        with open(self.npc_filename, 'r') as file:
+        with open(self.npc_filename, "r") as file:
             npc_data = json.load(file)
 
         npc_dict = {}
@@ -37,8 +43,8 @@ class GameLoader:
         return npc_dict
 
     def import_rooms(self):
-        # Method to import rooms 
-        with open(self.room_filename, 'r') as file:
+        # Method to import rooms
+        with open(self.room_filename, "r") as file:
             room_data = json.load(file)
 
         room_dict = {}
@@ -51,7 +57,7 @@ class GameLoader:
 
     def import_items(self):
         # Method to import items
-        with open(self.items_filename, 'r') as item_json:
+        with open(self.items_filename, "r") as item_json:
             data = json.load(item_json)
 
         item_dict = {}
@@ -64,7 +70,7 @@ class GameLoader:
 
     def import_puzzles(self):
         # Method to import puzzles
-        with open(self.puzzles_filename, 'r', encoding='utf-8') as puzzle_json:
+        with open(self.puzzles_filename, "r", encoding="utf-8") as puzzle_json:
             data = json.load(puzzle_json)
 
         puzzle_dict = {}
@@ -75,10 +81,9 @@ class GameLoader:
 
         return puzzle_dict
 
-  
     def import_messages(self):
         # Method to import messages
-        with open(self.messages_filename, 'r') as message_json:
+        with open(self.messages_filename, "r") as message_json:
             data = json.load(message_json)
 
         messages_dict = {}
